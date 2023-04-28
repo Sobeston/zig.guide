7005945e386193cba3769f3827d37fd5774b31f7
Sobeston <15335529+Sobeston@users.noreply.github.com>
2023-04-28 19:18:22 +0100

    properly update to zig master

--- do_tests.zig
+++ do_tests.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 
-const chapter_0 = @import("test-out/test-c0.zig");
-const chapter_1 = @import("test-out/test-c1.zig");
-const chapter_2 = @import("test-out/test-c2.zig");
-const chapter_3 = @import("test-out/test-c3.zig");
-const chapter_4 = @import("test-out/test-c4.zig");
-const chapter_5 = @import("test-out/test-c5.zig");
+pub const chapter_0 = @import("test-out/test-c0.zig");
+pub const chapter_1 = @import("test-out/test-c1.zig");
+pub const chapter_2 = @import("test-out/test-c2.zig");
+pub const chapter_3 = @import("test-out/test-c3.zig");
+pub const chapter_4 = @import("test-out/test-c4.zig");
+// pub const chapter_5 = @import("test-out/test-c5.zig"); -- disabled: no async support in compiler
 
 comptime {
-    _ = std.testing.refAllDecls(@This());
+    std.testing.refAllDecls(@This());
 }
