a71fdb00c68a5a1e9a08d03635001f03f6dbf7a7
Roman Frołow <rofrol@gmail.com>
2023-08-20 18:41:33 +0200

    update test-out

--- test-out/test-c2.zig
+++ test-out/test-c2.zig
@@ -154,35 +154,6 @@ test "io reader usage" {
     try expectEqualSlices(u8, contents, message);
 }
 
-fn nextLine(reader: anytype, buffer: []u8) !?[]const u8 {
-    var line = (try reader.readUntilDelimiterOrEof(
-        buffer,
-        '\n',
-    )) orelse return null;
-    // trim annoying windows-only carriage return character
-    if (@import("builtin").os.tag == .windows) {
-        return std.mem.trimRight(u8, line, "\r");
-    } else {
-        return line;
-    }
-}
-
-test "read until next line" {
-    const stdout = std.io.getStdOut();
-    const stdin = std.io.getStdIn();
-
-    try stdout.writeAll(
-        \\ Enter your name:
-    );
-
-    var buffer: [100]u8 = undefined;
-    const input = (try nextLine(stdin.reader(), &buffer)).?;
-    try stdout.writer().print(
-        "Your name is: \"{s}\"\n",
-        .{input},
-    );
-}
-
 // Don't create a type like this! Use an
 // arraylist with a fixed buffer allocator
 const MyByteList = struct {
