fe7ec89e170703211c9cf8598c60432d2a21f9ba
Sobeston <15335529+Sobeston@users.noreply.github.com>
2023-11-11 22:57:02 +0000

    split out more zig files

---
+++ website/docs/1-language-basics/15|enum-declarations.zig
@@ -0,0 +1,14 @@
+// hide-start
+const expect = @import("std").testing.expect;
+
+// hide-end
+const Mode = enum {
+    var count: u32 = 0;
+    on,
+    off,
+};
+
+test "hmm" {
+    Mode.count += 1;
+    try expect(Mode.count == 1);
+}
