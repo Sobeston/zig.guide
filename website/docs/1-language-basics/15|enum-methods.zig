3c130c39ed8cc80a2412db2b6c3e30f2b4defd84
Sobeston <15335529+Sobeston@users.noreply.github.com>
2023-11-11 22:57:02 +0000

    split out more zig files

---
+++ website/docs/1-language-basics/15|enum-methods.zig
@@ -0,0 +1,17 @@
+// hide-start
+const expect = @import("std").testing.expect;
+
+// hide-end
+const Suit = enum {
+    clubs,
+    spades,
+    diamonds,
+    hearts,
+    pub fn isClubs(self: Suit) bool {
+        return self == Suit.clubs;
+    }
+};
+
+test "enum method" {
+    try expect(Suit.spades.isClubs() == Suit.isClubs(.spades));
+}
