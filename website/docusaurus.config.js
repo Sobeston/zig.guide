d8ce23eba5c2023f636f06e74e330f1083d65a07
Sobeston <15335529+Sobeston@users.noreply.github.com>
2023-11-11 23:27:58 +0000

    fix edit links

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -45,7 +45,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Sobeston/ziglearn/tree/docusaurus/website/',
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
         },
@@ -54,7 +54,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Sobeston/ziglearn/tree/docusaurus/website/',
         },
         theme: {
           customCss: './src/css/custom.css',
