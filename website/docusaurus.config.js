5821a2e50cb5c2fab5a8f7a708952f1d1f4ce7d3
Sobeston <15335529+Sobeston@users.noreply.github.com>
2023-11-13 22:33:20 +0000

    respect respectPrefersColorScheme (default dark)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -66,6 +66,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
