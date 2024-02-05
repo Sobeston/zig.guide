6c7afc162bf289b25abdc28dbb696e47d6d94cf0
joshua.macy@gmail.com <majyc@users.noreply.github.com>
2024-02-05 14:03:58 -0500

    Update 03.filesystem-make-dir-iterable.zig
    cwd no longer has method openIterableDir, instead that is handled by passing .iterate = True as a parameter to the openDir

--- website/docs/02-standard-library/03.filesystem-make-dir-iterable.zig
+++ website/docs/02-standard-library/03.filesystem-make-dir-iterable.zig
@@ -5,18 +5,18 @@ const eql = std.mem.eql;
 // hide-end
 test "make dir" {
     try std.fs.cwd().makeDir("test-tmp");
-    var iter_dir = try std.fs.cwd().openIterableDir(
+    var iter_dir = try std.fs.cwd().openDir(
         "test-tmp",
-        .{},
+        .{ .iterate = true },
     );
     defer {
         iter_dir.close();
         std.fs.cwd().deleteTree("test-tmp") catch unreachable;
     }
 
-    _ = try iter_dir.dir.createFile("x", .{});
-    _ = try iter_dir.dir.createFile("y", .{});
-    _ = try iter_dir.dir.createFile("z", .{});
+    _ = try iter_dir.createFile("x", .{});
+    _ = try iter_dir.createFile("y", .{});
+    _ = try iter_dir.createFile("z", .{});
 
     var file_count: usize = 0;
     var iter = iter_dir.iterate();
