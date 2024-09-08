e5acb7ef0614c86b78cbffae4df4aa792c558530
Sonic <sonic.from.new.yoke@gmail.com>
2024-09-08 12:31:01 +0300

    chore: actualize build path in docs and project itself

--- website/versioned_docs/version-0.14/03-build-system/04.zig-build-hello/build.zig
+++ website/versioned_docs/version-0.14/03-build-system/04.zig-build-hello/build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "hello",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
     });
