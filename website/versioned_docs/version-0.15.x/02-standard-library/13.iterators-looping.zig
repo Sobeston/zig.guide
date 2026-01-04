3b6be6919efcdcbc8c94fbc0719c31155d33f6d8
Sobeston <15335529+Sobeston@users.noreply.github.com>
2026-01-05 06:52:32 +0800

    fix fd leak / closes #319

--- website/versioned_docs/version-0.15.x/02-standard-library/13.iterators-looping.zig
+++ website/versioned_docs/version-0.15.x/02-standard-library/13.iterators-looping.zig
@@ -3,12 +3,14 @@ const std = @import("std");
 const expect = std.testing.expect;
 // hide-end
 test "iterator looping" {
-    var iter = (try std.fs.cwd().openDir(
-        ".",
-        .{ .iterate = true },
-    )).iterate();
+    var cwd = try std.fs.cwd().openDir(".", .{
+        .iterate = true,
+    });
+    defer cwd.close();
 
     var file_count: usize = 0;
+
+    var iter = cwd.iterate();
     while (try iter.next()) |entry| {
         if (entry.kind == .file) file_count += 1;
     }
