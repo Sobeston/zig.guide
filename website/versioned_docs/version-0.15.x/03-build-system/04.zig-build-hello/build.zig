7df3982b6effbdf1420b2f196f07ee3360134f47
OFYA61 <62146434+OFYA61@users.noreply.github.com>
2026-01-21 21:38:15 +0100

    Update build.zig

--- website/versioned_docs/version-0.15.x/03-build-system/04.zig-build-hello/build.zig
+++ website/versioned_docs/version-0.15.x/03-build-system/04.zig-build-hello/build.zig
@@ -3,9 +3,11 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "hello",
-        .root_source_file = b.path("src/main.zig"),
-        .target = b.standardTargetOptions(.{}),
-        .optimize = b.standardOptimizeOption(.{}),
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.standardTargetOptions(.{},
+            .optimize = b.standardOptimizeOption(.{}),
+        }),
     });
 
     b.installArtifact(exe);
